77d5f355ca423802f5b7eb33a200591a61711bd5
Prashanth <prashanthhegde696@gmail.com>
2022-01-06 00:21:29 +0900

    agent modal title change
    agent agreement modal title change

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let cleantheroom = new Promise(function(resolve, reject){
+     let isclean = false;
+
+     if(isclean){
+         resolve('cleaned');
+     }else{
+         reject('not cleaned');
+     }
+
+
+});
+cleantheroom.then(function(fromresolve){
+    console.log('the room is' + fromresolve);
+}).catch(function(fromreject){
+    console.log('the room is' + fromreject)
+});
+    
+
